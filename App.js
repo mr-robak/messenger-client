3e0b5abf47d5398b0df691887651034eb36e40c1
Marcin Robak <mr.robak@gmail.com>
2020-07-04 19:25:54 +0200

    add queries to fetch and display user  data

--- App.js
+++ App.js
@@ -1,15 +1,18 @@
 import React from "react";
 // import { AppRegistry } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "@apollo/react-hooks";
-import HelloWorld from "./screens/HelloWorld";
-
-import { StyleSheet, Text, View } from "react-native";
+// import { InMemoryCache } from "apollo-cache-inmemory";
+// import { createHttpLink } from "apollo-link-http";
 import FetchData from "./screens/FetchData";
+
+// import HelloWorld from "./screens/HelloWorld";
 
 // Create the client as outlined in the setup guide
 const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql",
+  uri: "http://192.168.2.102:4000/graphql",
+  // cache: new InMemoryCache(),
 });
 
 export default function App() {
--- screens/FetchData.js
+++ screens/FetchData.js
@@ -1,21 +1,98 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { useQuery } from "@apollo/react-hooks";
+import gql from "graphql-tag";
+
+const GET_CHAT = gql`
+  query getCompleteChat($id: Int!) {
+    getCompleteChat(id: $id) {
+      id
+      title
+      creatorId
+      participants {
+        firstName
+        email
+      }
+      messages {
+        senderId
+        content
+        contentType
+      }
+    }
+  }
+`;
+const GET_USER = gql`
+  query getUser($id: Int!) {
+    user(id: $id) {
+      id
+      firstName
+      lastName
+      email
+      password
+      isLoggedin
+      preferences
+      user_contacts {
+        id
+        firstName
+        lastName
+      }
+    }
+  }
+`;
 
 export default function FetchData() {
+  // console.log(dataFetch);
+  const { loading, error, data } = useQuery(GET_USER, {
+    variables: { id: 1 },
+  });
+  console.log(data);
+
+  if (loading)
+    return (
+      <View style={s.container}>
+        <Text style={s.text2}>Loading...</Text>
+      </View>
+    );
+
+  if (error)
+    return (
+      <View style={s.container}>
+        <Text style={s.text2}>Error: {error.message}</Text>
+      </View>
+    );
+
   return (
-    <View style={styles.container}>
-      <Text>Fetch data!</Text>
+    <View style={s.container}>
+      <Text style={s.text1}>Hello {data.user.firstName}!</Text>
+      <Text style={s.text2}>Here are your contacts:</Text>
+      {data.user.user_contacts.map((user) => {
+        return (
+          <Text key={user.id} style={s.text3}>
+            {user.firstName}
+          </Text>
+        );
+      })}
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+const s = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
-    fontSize: 50,
+  },
+  text1: {
+    fontSize: 40,
+    fontWeight: "500",
+  },
+  text2: {
+    fontSize: 25,
+    fontWeight: "500",
+  },
+  text3: {
+    fontSize: 30,
     fontWeight: "500",
   },
 });
